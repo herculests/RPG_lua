b0bbe5f7b26434c869f2f0adb9d309ad0e054806
herculests <henriquesantos@alunos.utfpr.edu.br>
2019-11-14 15:04:15 -0300

    movimentos

--- main.lua
+++ main.lua
@@ -2,6 +2,8 @@ local mapa = {}
 local tile_grass
 local tile_water
 local tile_sand
+local posCol = 2
+local posLin = 2
 
 function LoadMap(Matriz)
     local file = io.open(Matriz, "r")
@@ -19,15 +21,21 @@ function LoadMap(Matriz)
 end
 
 function love.load()
-    love.window.setMode( 896, 608, {resizable=false} )
+    love.window.setMode( 1792, 608, {resizable=false} )
     LoadMap("RPG_lua/Matriz.txt")
-    muro = love.graphics.newImage("muro.png")
     heroi = love.graphics.newImage("heroi.png")
+    heroiW = love.graphics.newImage("heroiW.png")
+    heroiS = love.graphics.newImage("heroiS.png")
+    heroiA = love.graphics.newImage("heroiA.png")
+    heroiD = love.graphics.newImage("heroiD.png")
+    muro = love.graphics.newImage("muro.png")
+    img = love.graphics.newImage("img.png")
     portal = love.graphics.newImage("portal.png")
     piso = love.graphics.newImage("piso.png")
     feno = love.graphics.newImage("feno.png")
     buraco = love.graphics.newImage("buraco.png")
     bauFechado = love.graphics.newImage("baufechado.png")
+    bauAberto = love.graphics.newImage("bauAberto.png")
 end
 
 function love.draw()
@@ -36,16 +44,16 @@ function love.draw()
     do
         for coluna=1,37,1
         do
+            
             if(mapa[linha][coluna] == "X")
             then
                 love.graphics.draw(muro, tamanho*coluna-tamanho, tamanho*linha-tamanho)
+            elseif(mapa[linha][coluna] == "Y")
+            then
+                love.graphics.draw(piso,tamanho*coluna-tamanho, tamanho*linha-tamanho)
             elseif(mapa[linha][coluna] == "A")
             then
                 love.graphics.draw(feno, tamanho*coluna-tamanho, tamanho*linha-tamanho)
-            elseif(mapa[linha][coluna] == "H")
-            then
-                love.graphics.draw(piso,tamanho*coluna-tamanho, tamanho*linha-tamanho)
-                love.graphics.draw(heroi, tamanho*coluna-tamanho, tamanho*linha-tamanho)
             elseif(mapa[linha][coluna] == "C")
             then
                 love.graphics.draw(piso,tamanho*coluna-tamanho, tamanho*linha-tamanho)
@@ -54,14 +62,63 @@ function love.draw()
             then
                 love.graphics.draw(piso,tamanho*coluna-tamanho, tamanho*linha-tamanho)
                 love.graphics.draw(bauFechado, (tamanho*coluna-tamanho) + 3, (tamanho*linha-tamanho) + 3)
+            elseif(mapa[linha][coluna] == "BA")
+            then
+                love.graphics.draw(piso,tamanho*coluna-tamanho, tamanho*linha-tamanho)
+                love.graphics.draw(bauAberto, (tamanho*coluna-tamanho) + 3, (tamanho*linha-tamanho) + 3)
             elseif(mapa[linha][coluna] == "P")
             then
                 love.graphics.draw(piso,tamanho*coluna-tamanho, tamanho*linha-tamanho)
                 love.graphics.draw(portal, (tamanho*coluna-tamanho) + 1, (tamanho*linha-tamanho) + 1)
             else
                 love.graphics.draw(piso,tamanho*coluna-tamanho, tamanho*linha-tamanho)
+                love.graphics.draw(heroi, tamanho*posCol-tamanho, tamanho*posLin-tamanho)
             end
         end
     end
+    love.graphics.draw(img, 896, 0)
 end
-    
+
+function love.keypressed(key, scancode, isrepeat)
+    if key == "d" then
+        heroi = heroiD
+        if mapa[posLin][posCol + 1] == "Y"
+        then
+            posCol = posCol + 1
+        end
+        if mapa[posLin][posCol +1] == "B"
+        then
+            mapa[posLin][posCol +1] = "BA"
+        end
+    elseif key == "a" then
+        heroi = heroiA
+        if mapa[posLin][posCol - 1] == "Y"
+        then
+            posCol = posCol - 1
+        end
+        if mapa[posLin][posCol -1] == "B"
+        then
+            mapa[posLin][posCol -1] = "BA"
+        end
+    elseif key == "s" then
+        heroi = heroiS
+        if mapa[posLin + 1][posCol] == "Y"
+        then
+            posLin = posLin + 1
+        end
+        if mapa[posLin +1][posCol] == "B"
+        then
+            mapa[posLin +1][posCol] = "BA"
+        end
+    elseif key == "w" then
+        heroi = heroiW
+        if mapa[posLin - 1][posCol] == "Y"
+        then
+            posLin = posLin - 1
+        end
+        if mapa[posLin -1][posCol] == "B"
+        then
+            mapa[posLin -1][posCol] = "BA"
+        end
+    end
+ end
